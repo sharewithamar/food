249ccefc8a1e9e30ca997b638817aabdd4b2d0c4
Amarnath <sharewithamar@gmail.com>
2020-06-17 18:51:00 +0530

    displaying results by grouping price- base

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,30 +1,17 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
-
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose',
-        },
-      });
-      setResults(response.data.businesses);
-      setErrorMessage('');
-    } catch (err) {
-      setErrorMessage('Something went wrong');
-    }
+  const [searchApi, results, errorMessage] = useResults();
+  const filterResultsByPrice = (price) => {
+    //price === '$' || '$$' || '$$$'
+    return results.filter((result) => {
+      return result.price === price;
+    });
   };
-  //Cal searchApi when component is first rendered -Bad code
-  //searchApi('pasta');
   return (
     <View>
       <SearchBar
@@ -34,6 +21,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text> We have found {results.length} results</Text>
+      <ResultsList title='Cost Effective' results={filterResultsByPrice('$')} />
+      <ResultsList title='Bit Pricer' results={filterResultsByPrice('$$')} />
+      <ResultsList title='Big Spender' results={filterResultsByPrice('$$$')} />
     </View>
   );
 };
